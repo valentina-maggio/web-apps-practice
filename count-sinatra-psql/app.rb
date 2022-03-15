27616e97f6ee4fc6ab0161596e115be7eda1f7cb
Valentina <maggio.vale@gmail.com>
2022-03-15 12:29:34 +0000

    Add count sinatra psql exercise
    - create databases
    - add tables to databases

---
+++ count-sinatra-psql/app.rb
@@ -0,0 +1,19 @@
+require "sinatra"
+require "./lib/counter"
+require './database_connection_setup'
+
+class CounterApp < Sinatra::Base
+
+  before do
+    @counter = Counter.instance
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/increment' do
+    @counter.increment
+    redirect '/'
+  end
+end
